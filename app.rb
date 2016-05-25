40558e9cd229cf030d36b5d54eee45f65be50c0e
Diego Lugo <diego.lugo.06@gmail.com>
2016-05-25 04:50:28 -0700

    s3 upload with hidden vars

--- app.rb
+++ app.rb
@@ -1,26 +1,27 @@
 require 'sinatra'
 require 'bundler'
 require 'active_record'
+require 'aws-sdk'
 require_relative 'models/image'
 require_relative 'config/environments'
 
-get '/' do
-  @images = Image.all
-  erb :index
-end
-
 post '/' do
   if params[:file]
-    @filename = params[:file][:filename]
-    file = params[:file][:tempfile]
+    file       = params[:file][:tempfile]
+    filename   = params[:file][:filename]
 
-    File.open("./public/#{@filename}", 'wb') do |f|
-      f.write(file.read)
-      Image.create(
-      url: "/" + @filename,
-      title: @filename
-      )
-    end
+    s3 = Aws::S3::Resource.new(
+      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_KEY']),
+      region: ENV['AWS_REGION']
+    )
+    obj = s3.bucket(ENV['AWS_BUCKET']).object(filename)
+    obj.upload_file(file.path, acl:'public-read')
+
+
+    Image.create(
+      url: obj.public_url,
+      title: filename
+    )
 
     redirect '/'
   else
